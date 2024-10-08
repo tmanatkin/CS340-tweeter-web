f1c5a3b0cb3716b8a155289a5293b339372cb0f6
tanneratkin <tmanatkin@gmail.com>
2024-10-08 16:04:45 -0600

    cleanup

--- tweeter-web/src/components/appNavbar/AppNavbar.tsx
+++ tweeter-web/src/components/appNavbar/AppNavbar.tsx
@@ -2,7 +2,6 @@ import "./AppNavbar.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink, useLocation } from "react-router-dom";
 import Image from "react-bootstrap/Image";
-import { AuthToken } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "../userInfo/UserInfoHook";
 import { LogoutPresenter, LogoutView } from "../../presenters/LogoutPresenter";
